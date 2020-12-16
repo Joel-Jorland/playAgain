af4f51b07f9f82ba7cb59cf847902aba80988bc7
Nicolas Piriou <n.piriou@yahoo.fr>
2020-12-16 11:18:38 +0100

    Revert "added navbar & main section everywhere"
    This reverts commit 8820a97ed65fff62114cb47f283c7be7cb5a738b.

--- login.js
+++
@@ -1,5 +0,0 @@
-const login = () => {
-  const loginDiv = document.getElementById("login_div");
-  const loginId = document.getElementById("login_id").value;
-  loginDiv.innerHTML = `Bienvenue ${loginId}`;
-};
